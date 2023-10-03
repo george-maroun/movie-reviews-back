e0074304578ee1bc55fd30fd312caf98b4f9dde0
George M <gsmaroun@gmail.com>
2023-10-02 22:30:47 -0400

    Initial commit

---
+++ src/main/java/dev/gmaroun/movies/ReviewRepository.java
@@ -0,0 +1,11 @@
+package dev.gmaroun.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+}
